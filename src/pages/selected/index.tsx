bbe9621984bb7860583131aa21a01d13a3bf63c7
teamdev <pearl.k.sakura@gmail.com>
2025-08-11 20:35:54 +0700

    update selected cbam cfp

--- src/pages/selected/index.tsx
+++ src/pages/selected/index.tsx
@@ -14,21 +14,6 @@ const SelectPage = () => {
       <Navbar />
       <div className="max-w-5xl mt-20 mx-auto">
         <div
-          className="bg-primary/20 rounded-3xl px-8 hover:bg-primary/30 transition w-fit mx-auto flex gap-3 my-10 cursor-pointer"
-          onClick={() => navigate(PROTECTED_PATH.DASHBOARD)}
-        >
-          <img src="./Climate change-rafiki.svg" className="w-60" />
-          <div className="mt-auto mb-9">
-            <p className="text-3xl font-bold mt-5 mb-3 text-primary">
-              เข้าสู่หน้าหลัก CFP
-            </p>
-            <p className="w-72 text-sm">
-              ระบบคำนวณและทวนสอบคาร์บอนฟุตพริ้นท์ของผลิตภัณฑ์ (Carbon Footprint
-              of Products)
-            </p>
-          </div>
-        </div>
-        <div
           className="bg-secondary-500/20 rounded-3xl px-8 hover:bg-secondary-500/30  transition flex mx-auto w-fit gap-3 cursor-pointer"
           onClick={() => {
             const cbamWindow = window.open(server);
@@ -51,6 +36,21 @@ const SelectPage = () => {
               ระบบคำนวณ Embedded emission
               ภายใต้ข้อกำหนดของมาตรการการปรับภาษีคาร์บอนข้ามพรมแดนของสหภาพยุโรป
               (Carbon Border Adjustment Mechanism)
+            </p>
+          </div>
+        </div>
+        <div
+          className="bg-primary/20 rounded-3xl px-8 hover:bg-primary/30 transition w-fit mx-auto flex gap-3 my-10 cursor-pointer"
+          onClick={() => navigate(PROTECTED_PATH.DASHBOARD)}
+        >
+          <img src="./Climate change-rafiki.svg" className="w-60" />
+          <div className="mt-auto mb-9">
+            <p className="text-3xl font-bold mt-5 mb-3 text-primary">
+              เข้าสู่หน้าหลัก CFP
+            </p>
+            <p className="w-72 text-sm">
+              ระบบคำนวณและทวนสอบคาร์บอนฟุตพริ้นท์ของผลิตภัณฑ์ (Carbon Footprint
+              of Products)
             </p>
           </div>
         </div>
